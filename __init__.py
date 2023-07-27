7519d2645516349c373ab692818ff631a73f0cb5
SeargeDP <searge@ocean-labs.de>
2023-07-27 12:12:34 +0200

    Added SeargeSDXLBasePromptEncoder, SeargeSDXLRefinerPromptEncoder, and SeargeIntegerScaler node types

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .searge_sdxl_sampler_node import NODE_CLASS_MAPPINGS
+from .searge_sdxl_sampler_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__all__ = ['NODE_CLASS_MAPPINGS']
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
--- searge_sdxl_sampler_node.py
+++ searge_sdxl_sampler_node.py
@@ -28,7 +28,6 @@ SOFTWARE.
 
 import comfy.samplers
 import nodes
-import torch
 
 from nodes import MAX_RESOLUTION
 
@@ -47,8 +46,8 @@ class SeargeSDXLSampler:
                     "refiner_negative": ("CONDITIONING", ),
                     "latent_image": ("LATENT", ),
                     "noise_seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
-                    "steps": ("INT", {"default": 30, "min": 1, "max": 10000}),
-                    "cfg": ("FLOAT", {"default": 7.0, "min": 0.0, "max": 100.0}),
+                    "steps": ("INT", {"default": 30, "min": 1, "max": 1000}),
+                    "cfg": ("FLOAT", {"default": 7.0, "min": 0.0, "max": 100.0, "step": 0.5}),
                     "sampler_name": (comfy.samplers.KSampler.SAMPLERS, {"default": "dpmpp_2m"}),
                     "scheduler": (comfy.samplers.KSampler.SCHEDULERS, {"default": "karras"}),
                     "base_ratio": ("FLOAT", {"default": 0.8, "min": 0.0, "max": 1.0, "step": 0.01}),
@@ -56,7 +55,8 @@ class SeargeSDXLSampler:
                     },
                 }
 
-    RETURN_TYPES = ("LATENT",)
+    RETURN_TYPES = ("LATENT", )
+    RETURN_NAMES = ("", )
     FUNCTION = "sample"
 
     CATEGORY = "Searge/SDXL"
@@ -89,20 +89,21 @@ class SeargeSDXLPromptEncoder:
                     "neg_g": ("STRING", {"multiline": True, "default": "NEG_G"}),
                     "neg_l": ("STRING", {"multiline": True, "default": "NEG_L"}),
                     "neg_r": ("STRING", {"multiline": True, "default": "NEG_R"}),
-                    "base_width": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION}),
-                    "base_height": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION}),
-                    "crop_w": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION}),
-                    "crop_h": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION}),
-                    "target_width": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION}),
-                    "target_height": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION}),
+                    "base_width": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "base_height": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "crop_w": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "crop_h": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "target_width": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "target_height": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
                     "pos_ascore": ("FLOAT", {"default": 6.0, "min": 0.0, "max": 1000.0, "step": 0.01}),
                     "neg_ascore": ("FLOAT", {"default": 2.5, "min": 0.0, "max": 1000.0, "step": 0.01}),
-                    "refiner_width": ("INT", {"default": 2048.0, "min": 0, "max": MAX_RESOLUTION}),
-                    "refiner_height": ("INT", {"default": 2048.0, "min": 0, "max": MAX_RESOLUTION}),
+                    "refiner_width": ("INT", {"default": 2048.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "refiner_height": ("INT", {"default": 2048.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
                     },
                 }
 
     RETURN_TYPES = ("CONDITIONING", "CONDITIONING", "CONDITIONING", "CONDITIONING", )
+    RETURN_NAMES = ("base_positive", "base_negative", "refiner_positive", "refiner_negative", )
     FUNCTION = "encode"
 
     CATEGORY = "Searge/SDXL"
@@ -140,14 +141,109 @@ class SeargeSDXLPromptEncoder:
         # positive refiner prompt
         tokens3 = refiner_clip.tokenize(pos_r)
         cond3, pooled3 = refiner_clip.encode_from_tokens(tokens3, return_pooled=True)
-        res3 = [[cond3, {"pooled_output": pooled3, "aesthetic_score": pos_ascore, "width": refiner_width,"height": refiner_height}]]
+        res3 = [[cond3, {"pooled_output": pooled3, "aesthetic_score": pos_ascore, "width": refiner_width, "height": refiner_height}]]
 
         # negative refiner prompt
         tokens4 = refiner_clip.tokenize(neg_r)
         cond4, pooled4 = refiner_clip.encode_from_tokens(tokens4, return_pooled=True)
-        res4 = [[cond4, {"pooled_output": pooled4, "aesthetic_score": neg_ascore, "width": refiner_width,"height": refiner_height}]]
+        res4 = [[cond4, {"pooled_output": pooled4, "aesthetic_score": neg_ascore, "width": refiner_width, "height": refiner_height}]]
 
         return (res1, res2, res3, res4, )
+
+
+# SDXL CLIP Text Encoder for base prompts
+
+class SeargeSDXLBasePromptEncoder:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                    "base_clip": ("CLIP", ),
+                    "pos_g": ("STRING", {"multiline": True, "default": "POS_G"}),
+                    "pos_l": ("STRING", {"multiline": True, "default": "POS_L"}),
+                    "neg_g": ("STRING", {"multiline": True, "default": "NEG_G"}),
+                    "neg_l": ("STRING", {"multiline": True, "default": "NEG_L"}),
+                    "base_width": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "base_height": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "crop_w": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "crop_h": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "target_width": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "target_height": ("INT", {"default": 4096.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    },
+                }
+
+    RETURN_TYPES = ("CONDITIONING", "CONDITIONING", )
+    RETURN_NAMES = ("base_positive", "base_negative", )
+    FUNCTION = "encode"
+
+    CATEGORY = "Searge/SDXL"
+
+    def encode(self, base_clip, pos_g, pos_l, neg_g, neg_l, base_width, base_height, crop_w, crop_h, target_width, target_height, ):
+        empty = base_clip.tokenize("")
+
+        # positive base prompt
+        tokens1 = base_clip.tokenize(pos_g)
+        tokens1["l"] = base_clip.tokenize(pos_l)["l"]
+
+        if len(tokens1["l"]) != len(tokens1["g"]):
+            while len(tokens1["l"]) < len(tokens1["g"]):
+                tokens1["l"] += empty["l"]
+            while len(tokens1["l"]) > len(tokens1["g"]):
+                tokens1["g"] += empty["g"]
+
+        cond1, pooled1 = base_clip.encode_from_tokens(tokens1, return_pooled=True)
+        res1 = [[cond1, {"pooled_output": pooled1, "width": base_width, "height": base_height, "crop_w": crop_w, "crop_h": crop_h, "target_width": target_width, "target_height": target_height}]]
+
+        # negative base prompt
+        tokens2 = base_clip.tokenize(neg_g)
+        tokens2["l"] = base_clip.tokenize(neg_l)["l"]
+
+        if len(tokens2["l"]) != len(tokens2["g"]):
+            while len(tokens2["l"]) < len(tokens2["g"]):
+                tokens2["l"] += empty["l"]
+            while len(tokens2["l"]) > len(tokens2["g"]):
+                tokens2["g"] += empty["g"]
+
+        cond2, pooled2 = base_clip.encode_from_tokens(tokens2, return_pooled=True)
+        res2 = [[cond2, {"pooled_output": pooled2, "width": base_width, "height": base_height, "crop_w": crop_w, "crop_h": crop_h, "target_width": target_width, "target_height": target_height}]]
+
+        return (res1, res2, )
+
+
+# SDXL CLIP Text Encoder for prompts with base and refiner support
+
+class SeargeSDXLRefinerPromptEncoder:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                    "refiner_clip": ("CLIP", ),
+                    "pos_r": ("STRING", {"multiline": True, "default": "POS_R"}),
+                    "neg_r": ("STRING", {"multiline": True, "default": "NEG_R"}),
+                    "pos_ascore": ("FLOAT", {"default": 6.0, "min": 0.0, "max": 1000.0, "step": 0.01}),
+                    "neg_ascore": ("FLOAT", {"default": 2.5, "min": 0.0, "max": 1000.0, "step": 0.01}),
+                    "refiner_width": ("INT", {"default": 2048.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    "refiner_height": ("INT", {"default": 2048.0, "min": 0, "max": MAX_RESOLUTION, "step": 64}),
+                    },
+                }
+
+    RETURN_TYPES = ("CONDITIONING", "CONDITIONING", )
+    RETURN_NAMES = ("refiner_positive", "refiner_negative", )
+    FUNCTION = "encode"
+
+    CATEGORY = "Searge/SDXL"
+
+    def encode(self, refiner_clip, pos_r, neg_r, pos_ascore, neg_ascore, refiner_width, refiner_height, ):
+
+        # positive refiner prompt
+        tokens1 = refiner_clip.tokenize(pos_r)
+        cond1, pooled1 = refiner_clip.encode_from_tokens(tokens1, return_pooled=True)
+        res1 = [[cond1, {"pooled_output": pooled1, "aesthetic_score": pos_ascore, "width": refiner_width, "height": refiner_height}]]
+
+        # negative refiner prompt
+        tokens2 = refiner_clip.tokenize(neg_r)
+        cond2, pooled2 = refiner_clip.encode_from_tokens(tokens2, return_pooled=True)
+        res2 = [[cond2, {"pooled_output": pooled2, "aesthetic_score": neg_ascore, "width": refiner_width, "height": refiner_height}]]
+
+        return (res1, res2, )
 
 
 # Tool: text input node for prompt text
@@ -160,7 +256,8 @@ class SeargePromptText:
                     },
                 }
 
-    RETURN_TYPES = ("STRING",)
+    RETURN_TYPES = ("STRING", )
+    RETURN_NAMES = ("prompt", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Prompting"
@@ -181,7 +278,8 @@ class SeargePromptCombiner:
                     },
                 }
 
-    RETURN_TYPES = ("STRING",)
+    RETURN_TYPES = ("STRING", )
+    RETURN_NAMES = ("combined prompt", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Prompting"
@@ -209,7 +307,8 @@ class SeargeIntegerConstant:
                     },
                 }
 
-    RETURN_TYPES = ("INT",)
+    RETURN_TYPES = ("INT", )
+    RETURN_NAMES = ("value", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Integers"
@@ -229,7 +328,8 @@ class SeargeIntegerPair:
                     },
                 }
 
-    RETURN_TYPES = ("INT","INT",)
+    RETURN_TYPES = ("INT", "INT", )
+    RETURN_NAMES = ("value 1", "value 2", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Integers"
@@ -253,7 +353,8 @@ class SeargeIntegerMath:
                     },
                 }
 
-    RETURN_TYPES = ("INT",)
+    RETURN_TYPES = ("INT", )
+    RETURN_NAMES = ("result", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Integers"
@@ -273,6 +374,28 @@ class SeargeIntegerMath:
         return (int(res),)
 
 
+# Tool: integer scaler
+
+class SeargeIntegerScaler:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                    "value": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                    "factor": ("FLOAT", {"default": 1.0, "step": 0.01}),
+                    "multiple_of": ("INT", {"default": 1, "min": 0, "max": 65536}),
+                    },
+                }
+
+    RETURN_TYPES = ("INT", )
+    RETURN_NAMES = ("value", )
+    FUNCTION = "get_value"
+
+    CATEGORY = "Searge/Integers"
+
+    def get_value(self, value, factor, multiple_of, ):
+        return (int(value * factor // multiple_of) * multiple_of, )
+
+
 # Tool: float constant
 
 class SeargeFloatConstant:
@@ -283,7 +406,8 @@ class SeargeFloatConstant:
                     },
                 }
 
-    RETURN_TYPES = ("FLOAT",)
+    RETURN_TYPES = ("FLOAT", )
+    RETURN_NAMES = ("value", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Floats"
@@ -303,7 +427,8 @@ class SeargeFloatPair:
                     },
                 }
 
-    RETURN_TYPES = ("FLOAT","FLOAT",)
+    RETURN_TYPES = ("FLOAT", "FLOAT", )
+    RETURN_NAMES = ("value 1", "value 2", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Floats"
@@ -327,7 +452,8 @@ class SeargeFloatMath:
                     },
                 }
 
-    RETURN_TYPES = ("FLOAT",)
+    RETURN_TYPES = ("FLOAT", )
+    RETURN_NAMES = ("result", )
     FUNCTION = "get_value"
 
     CATEGORY = "Searge/Floats"
@@ -352,6 +478,8 @@ class SeargeFloatMath:
 NODE_CLASS_MAPPINGS = {
     "SeargeSDXLSampler": SeargeSDXLSampler,
     "SeargeSDXLPromptEncoder": SeargeSDXLPromptEncoder,
+    "SeargeSDXLBasePromptEncoder": SeargeSDXLBasePromptEncoder,
+    "SeargeSDXLRefinerPromptEncoder": SeargeSDXLRefinerPromptEncoder,
 
     "SeargePromptText": SeargePromptText,
     "SeargePromptCombiner": SeargePromptCombiner,
@@ -359,6 +487,7 @@ NODE_CLASS_MAPPINGS = {
     "SeargeIntegerConstant": SeargeIntegerConstant,
     "SeargeIntegerPair": SeargeIntegerPair,
     "SeargeIntegerMath": SeargeIntegerMath,
+    "SeargeIntegerScaler": SeargeIntegerScaler,
 
     "SeargeFloatConstant": SeargeFloatConstant,
     "SeargeFloatPair": SeargeFloatPair,
@@ -369,17 +498,20 @@ NODE_CLASS_MAPPINGS = {
 # Human readable names for the nodes
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SeargeSDXLSampler": "Sampler for SDXL (by Searge)",
-    "SeargeSDXLPromptEncoder": "SDXL Prompt Encoder (by Searge)",
+    "SeargeSDXLSampler": "Sampler for SDXL (SeargeSDXL)",
+    "SeargeSDXLPromptEncoder": "SDXL Prompt Encoder (SeargeSDXL)",
+    "SeargeSDXLBasePromptEncoder": "SDXL Base Prompt Encoder (SeargeSDXL)",
+    "SeargeSDXLRefinerPromptEncoder": "SDXL Refiner Prompt Encoder (SeargeSDXL)",
 
-    "SeargePromptText": "Prompt text input (by Searge)",
-    "SeargePromptCombiner": "Prompt combiner (by Searge)",
+    "SeargePromptText": "Prompt text input (SeargeSDXL)",
+    "SeargePromptCombiner": "Prompt combiner (SeargeSDXL)",
 
-    "SeargeIntegerConstant": "Integer Constant (by Searge)",
-    "SeargeIntegerPair": "Integer Pair (by Searge)",
-    "SeargeIntegerMath": "Integer Math (by Searge)",
+    "SeargeIntegerConstant": "Integer Constant (SeargeSDXL)",
+    "SeargeIntegerPair": "Integer Pair (SeargeSDXL)",
+    "SeargeIntegerMath": "Integer Math (SeargeSDXL)",
+    "SeargeIntegerScaler": "Integer Scaler (SeargeSDXL)",
 
-    "SeargeFloatConstant": "Float Constant (by Searge)",
-    "SeargeFloatPair": "Float Pair (by Searge)",
-    "SeargeFloatMath": "Float Math (by Searge)",
+    "SeargeFloatConstant": "Float Constant (SeargeSDXL)",
+    "SeargeFloatPair": "Float Pair (SeargeSDXL)",
+    "SeargeFloatMath": "Float Math (SeargeSDXL)",
 }
